daba7f8055a96bd524ab2206c99223f35cddb576
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:00:01 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan1/PBO210118085Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan1;
+
+/**
+ * @author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Perintah menampilkan value ke layar
+ * @author RISKY
+ */
+public class PBO210118085Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR"  +  "\n"  +  "_________________");
+        System.out.println("NAMA\t\t: RISKY IRFANSYAH");
+        System.out.println("ALAMAT\t\t: JL LURAH II NO 186");
+        System.out.println("TEMPAT LAHIR\t: CIMAHI");
+        System.out.println("TANGGAL LAHIR\t: 14 JUNI 1999");
+        System.out.println("_________________");
+        System.out.println("HOBI\t\t: TIDUR");
+        System.out.println("MAKES\t\t: SEMUA SUKA");
+        System.out.println("MIKES\t\t: SEMUA SUKA");
+    }
+    
+}
